9c9fe05e70966a853d791f7fb8dbe9d08525a46b
Stu Kennedy <info@continuata.com>
2016-08-15 14:36:24 +0100

    update libraries

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,30 @@
-var webpack = require('webpack');
+var webpack = require('webpack')
+
+var path = require('path')
 
 // postcss plugins
-var cssimport = require('postcss-import');
-var customProperties = require('postcss-custom-properties');
-var autoprefixer = require('autoprefixer');
-var csswring = require('csswring');
-var cssnested = require('postcss-nested');
+var cssimport = require('postcss-import')
+var customProperties = require('postcss-custom-properties')
+var autoprefixer = require('autoprefixer')
+var csswring = require('csswring')
+var cssnested = require('postcss-nested')
+
+var sassParams = [
+  'outputStyle=expanded',
+  'includePaths[]=' + path.resolve(__dirname, '../app/scss'),
+  'includePaths[]=' + path.resolve(__dirname, '../node_modules')
+]
+
+var sassLoader = [
+  'style-loader',
+  'css-loader',
+  'postcss-loader',
+  'sass-loader?' + sassParams.join('&')
+].join('!')
 
 module.exports = {
   entry: {
-    app : ['./src/index.js']
+    app: ['./src/index.js']
   },
   output: {
     path: __dirname + '/build/',
@@ -19,16 +34,36 @@ module.exports = {
   debug: true,
   plugins: [
     new webpack.ProvidePlugin({
-      riot: 'riot'
+      riot: 'riot',
+      Chart: 'chart.js'
     })
   ],
   module: {
     preLoaders: [
-      { test: /\.tag$/, exclude: /node_modules/, loader: 'riotjs-loader', query: { type: 'es6' } }
+      { test: /\.tag$/, exclude: /node_modules/, loader: 'riotjs-loader', query: { type: 'none' } }
     ],
     loaders: [
-      { test: /\.js|\.tag$/, exclude: /node_modules/, include: /src/, loader: 'babel-loader', query: {modules: 'common'} },
-      { test: /\.css$/, loader: 'style-loader!css-loader!postcss-loader' }
+      { test: /\.js|\.tag$/, exclude: /node_modules/, include: /src/, loader: 'babel', query: {presets: ['es2015', 'stage-1']} },
+      { test: /\.css$/, loader: 'style-loader!css-loader!postcss-loader' },
+      {
+        test: /\.jpe?g$|\.gif$|\.png$|\.ico|\.svg$|\.woff$|\.woff2$|\.eot$|\.otf$|\.ttf$/,
+        loader: 'file-loader?name=[path][name].[ext]'
+      },
+      { test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: 'url-loader?limit=10000&minetype=application/font-woff' },
+      { test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: 'file-loader' },
+      {
+        test: /\.json$/,
+        exclude: /node_modules/,
+        loader: 'json-loader'
+      },
+      {
+        test: /\.scss$/,
+        loader: sassLoader
+      },
+      {
+        test: /\.styl$/,
+        loader: 'css-loader!stylus-loader?paths=node_modules/bootstrap-stylus/stylus/'
+      }
     ]
   },
   postcss: [cssimport, cssnested, customProperties, autoprefixer, csswring],
@@ -38,4 +73,4 @@ module.exports = {
     hot: true,
     inline: true
   }
-};
+}
